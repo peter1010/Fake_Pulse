941f5deedd46cbe1960f0bc68a1876be61cbf2e5
Peter <peter1010@users.noreply.github.com>
2017-12-05 00:04:54 +0000

    Added missing functions

--- shim.c
+++ shim.c
@@ -13,8 +13,8 @@
 #include <stdlib.h>
 
 #include "logging.h"
+#include "originals.h"
 
-#define _UNUSED __attribute__((unused))
 
 extern void * __libc_dlsym(void * handle, const char * symbol);
 extern void * _dl_sym(void * handle, const char * symbol, void * callee);
@@ -22,16 +22,27 @@ extern void * _dl_sym(void * handle, const char * symbol, void * callee);
 void * libPulseHandle = NULL;
 
 #define MAGIC_HND (void *)(0x1865FE45)
+
 /**
- * 
+ * Intercept the call to dlopen() 
  *
  */
 void * dlopen(const char * filename, int flags)
 {
-    void * handle = dlmopen(LM_ID_BASE, filename, flags);
+    static void * (*the_real_dlopen)(const char * filename, int flags) = NULL;
+
+    if(the_real_dlopen == NULL) {
+        the_real_dlopen = dlsym(RTLD_NEXT, "dlopen");
+        if(the_real_dlopen == NULL) {
+            DEBUG_MSG("Failed to find dlopen()");
+            return NULL;
+        }
+    }
+
+    void * handle = the_real_dlopen(filename, flags);
 
     if(filename && (strncmp("libpulse.so", filename, 11) == 0)) {
-        DEBUG_MSG("dlopen called for %s", filename);
+        DEBUG_MSG("dlopen(%s) called", filename);
         libPulseHandle = handle;
         handle = MAGIC_HND;
     }
@@ -44,60 +55,155 @@ int pa_dummy()
     return 0;
 }
 
+/**
+ * Intercept the call to dlsym()
+ *
+ */
 void * dlsym(void * handle, const char * symbol)
 {
     void * address = NULL;
-    static void * (*the_real_one)(void * handle, const char *symbol) = NULL;
+    static void * (*the_real_dlsym)(void * handle, const char *symbol) = NULL;
 
-    if(the_real_one == NULL) {
-//        the_real_one = __libc_dlsym(RTLD_NEXT, "dlsym");
-        the_real_one = _dl_sym(RTLD_NEXT, "dlsym", dlsym);
+    if(the_real_dlsym == NULL) {
+        the_real_dlsym = _dl_sym(RTLD_NEXT, "dlsym", dlsym);
+        if(the_real_dlsym == NULL) {
+            DEBUG_MSG("Failed to find dlsym()");
+            return NULL;
+        }
     }
 
+    if(strcmp(symbol, "dlsym") == 0) {
+        // Avoid recursive call
+        address = dlsym;
 
-    if(handle == MAGIC_HND) {
-        address = the_real_one(libPulseHandle, symbol);
-        DEBUG_MSG("dlsym called for %s = %p", symbol, address);
+    } else if(handle == MAGIC_HND) {
+        // Symbol from libpulse?
+
+        address = the_real_dlsym(libPulseHandle, symbol);
+        DEBUG_MSG("dlsym(%s) called", symbol);
+
         if(address) {
             char buf[100];
             strcpy(buf, symbol);
             buf[0] = 'z';
             buf[1] = 'z';
-            void ** address2 = the_real_one(RTLD_DEFAULT, buf);
-            if(address2) {
-                *address2 = address;
+            void ** pSaveAddress = the_real_dlsym(RTLD_DEFAULT, buf);
+            if(pSaveAddress) {
+                *pSaveAddress = address;
+            } else {
+                DEBUG_MSG("WARNING: Save Symbol %s missing", buf);
             }
         }
-        address = the_real_one(RTLD_DEFAULT, symbol);
+        address = the_real_dlsym(RTLD_DEFAULT, symbol);
         if(address == NULL) {
             address = pa_dummy;
+            DEBUG_MSG("WARNING: Shim function  %s missing", symbol);
         }
     } else {
-        address = the_real_one(handle, symbol);
+        address = the_real_dlsym(handle, symbol);
     }
     return address;
 
 }
 
+/**
+ * Intercept the dlclose method
+ */
 int dlclose(void * handle)
 {
-    static int (*the_real_one)(void * handle) = NULL;
+    static int (*the_real_dlclose)(void * handle) = NULL;
+    
+    if(the_real_dlclose == NULL) {
+        the_real_dlclose = dlsym(RTLD_NEXT, "dlclose");
+        if(the_real_dlclose == NULL) {
+            DEBUG_MSG("Failed to find dlclose()");
+            return 0;
+        }
+    }
 
     if(handle == MAGIC_HND) {
         DEBUG_MSG("dlclose called");
         handle = libPulseHandle;
     } 
-    if(the_real_one == NULL) {
-        the_real_one = dlsym(RTLD_NEXT, "dlclose");
-    }
-    return the_real_one(handle);
+    return the_real_dlclose(handle);
 }
 
-#if 0
-char * getenv(const char * name)
+#define ZZ_NAME(name) zz##name
+
+#define LOAD_SYM(name) dlsym(handle, "zz_" #name)
+
+/**
+ * Get pulse symbols for the case when pulse has linked dynamically
+ * (as opposed to opened with dlopen)
+ */
+void init_symbols(void)
 {
-    fprintf(stderr, "stderr: getenv called for %s\n", name);
-    fflush(stderr);
-    return NULL;
+    if(zz_get_library_version == NULL) {
+        void * handle = dlopen("libpulse.so", RTLD_LAZY);
+
+	LOAD_SYM(get_library_version);
+	LOAD_SYM(channel_map_can_balance);
+	LOAD_SYM(channel_map_init);
+	LOAD_SYM(context_connect);
+	LOAD_SYM(context_disconnect);
+	LOAD_SYM(context_drain);
+	LOAD_SYM(context_get_server_info);
+	LOAD_SYM(context_get_sink_info_by_name);
+	LOAD_SYM(context_get_sink_info_list);
+	LOAD_SYM(context_get_sink_input_info);
+	LOAD_SYM(context_get_source_info_list);
+	LOAD_SYM(context_get_state);
+	LOAD_SYM(context_new);
+	LOAD_SYM(context_rttime_new);
+	LOAD_SYM(context_set_sink_input_volume);
+	LOAD_SYM(context_set_state_callback);
+	LOAD_SYM(context_unref);
+	LOAD_SYM(cvolume_set);
+	LOAD_SYM(cvolume_set_balance);
+	LOAD_SYM(frame_size);
+	LOAD_SYM(operation_get_state);
+	LOAD_SYM(operation_unref);
+	LOAD_SYM(proplist_gets);
+	LOAD_SYM(rtclock_now);
+	LOAD_SYM(stream_cancel_write);
+	LOAD_SYM(stream_connect_playback);
+	LOAD_SYM(stream_cork);
+	LOAD_SYM(stream_disconnect);
+	LOAD_SYM(stream_get_channel_map);
+	LOAD_SYM(stream_get_index);
+	LOAD_SYM(stream_get_latency);
+	LOAD_SYM(stream_get_sample_spec);
+	LOAD_SYM(stream_get_state);
+	LOAD_SYM(stream_get_time);
+	LOAD_SYM(stream_new);
+	LOAD_SYM(stream_set_state_callback);
+	LOAD_SYM(stream_set_write_callback);
+	LOAD_SYM(stream_unref);
+	LOAD_SYM(stream_update_timing_info);
+	LOAD_SYM(stream_write);
+	LOAD_SYM(sw_volume_from_linear);
+	LOAD_SYM(threaded_mainloop_free);
+	LOAD_SYM(threaded_mainloop_get_api);
+	LOAD_SYM(threaded_mainloop_in_thread);
+	LOAD_SYM(threaded_mainloop_lock);
+	LOAD_SYM(threaded_mainloop_new);
+	LOAD_SYM(threaded_mainloop_signal);
+	LOAD_SYM(threaded_mainloop_start);
+	LOAD_SYM(threaded_mainloop_stop);
+	LOAD_SYM(threaded_mainloop_unlock);
+	LOAD_SYM(threaded_mainloop_wait);
+	LOAD_SYM(usec_to_bytes);
+        LOAD_SYM(stream_set_read_callback);
+	LOAD_SYM(stream_connect_record);
+	LOAD_SYM(stream_readable_size);
+        LOAD_SYM(stream_writable_size);
+	LOAD_SYM(stream_drop);
+	LOAD_SYM(stream_get_buffer_attr);
+	LOAD_SYM(stream_get_device_name);
+	LOAD_SYM(context_set_subscribe_callback);
+	LOAD_SYM(context_subscribe);
+	LOAD_SYM(mainloop_api_once);
+        dlclose(handle);
+    }
 }
-#endif
+
